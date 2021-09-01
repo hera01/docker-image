6dbc481dee2ded313b2688b698e77cb849dd43cc
hera01 <68726463+hera01@users.noreply.github.com>
2021-09-01 17:46:45 +0530

    Initial commit

---
+++ src/main/java/com/docker/dockerspring1bot/DockerSpring1BotApplication.java
@@ -0,0 +1,13 @@
+package com.docker.dockerspring1bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpring1BotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpring1BotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/dockerspring1bot/DockerSpring1BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.dockerspring1bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpring1BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
